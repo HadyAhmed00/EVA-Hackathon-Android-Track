f26e574e056e8d813a502bbb34ec08297266adc5
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-06-10 15:13:11 +0200

    Update main.cpp

--- Problem5/main.cpp
+++ Problem5/main.cpp
@@ -16,30 +16,29 @@ int findNumber(int a, int b)
 
     queue<Node> q;
 
-    // Initially queue is empty
+ 
     Node temp = Node{ 0, 0, "" };
 
-    // Initialize visited to 1
+
     visited[0][0] = 1;
 
-    // Push temp in queue
+
     q.push(temp);
 
-    // While queue is not empty
+
     while (!q.empty()) {
 
-        // Get the front of the queue and pop it
+       
         Node u = q.front();
         q.pop();
 
-        // If popped element is the required number
+        
         if (u.a == 0 && u.b == b)
 
-            // Parse int from string and return it
+          
             return std::stoi(u.str);
 
-        // Loop for each digit and check the sum
-        // If not visited then push it to the queue
+
         for (int i = 0; i < 10; i++) {
             int dd = (u.a * 10 + i) % a;
             int ss = u.b + i;
@@ -50,7 +49,7 @@ int findNumber(int a, int b)
         }
     }
 
-    // Required number not found return -1.
+    
     return -1;
 }
 
